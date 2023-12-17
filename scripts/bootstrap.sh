fdf86cd0d5b6fcd09e296cefea5667737110f334
Logan Mancuso <mickeyacejr@live.com>
2023-12-17 15:43:44 -0500

    deploy usng the new bootstrap instead of ansible

---
+++ scripts/bootstrap.sh
@@ -0,0 +1,76 @@
+#!/bin/bash
+##############################################################################
+#
+# Author: Logan Mancuso
+# Created: 11.10.2023
+#
+##############################################################################
+
+# Redirect all output to log file
+exec > >(tee -a "${log_dst}") 2>&1
+
+# Function to map arguments to local variables
+function map_arguments() {
+  OPTS=$(getopt -o p1:p2: --long param1:param2: -- "$@")
+  if [ $? != 0 ] ; then echo "Failed parsing options." >&2 ; exit 1 ; fi
+  eval set -- "$OPTS"
+  while true; do
+    case "$1" in
+      -p1 | --param1 ) PARAM1="$2"; shift ;;
+      -p2 | --param2 ) PARAM2="$2"; shift ;;
+      -- ) shift; break ;;
+      * ) break ;;
+    esac
+  done
+  echo "Param 1 $PARAM1"
+  echo "Param 2 $PARAM2"
+  echo -e "END:\tmap_arguments"
+}
+
+# Helper function
+function helper() {
+  # Add the logic that the script will perform here
+  echo -e "START:\thelper"
+  # install helper tools
+  apt install -y neovim python3-full python3-pip python3-jsondiff
+  mkdir -p /var/log/tofu
+  chown -R root:root /var/log/tofu
+  chmod -R u+rwx,g+rwx /var/log/tofu
+  mkdir -p /opt/tofu/
+  chown -R root:root /opt/tofu
+  chmod -R u+rwx,g+rwx /opt/tofu
+  apt install -y git unzip wget fontconfig
+  mkdir -p ~/.local/share/fonts
+  wget -O ~/.local/share/fonts/CascadiaCode.zip https://github.com/ryanoasis/nerd-fonts/releases/download/v3.0.2/CascadiaCode.zip
+  unzip ~/.local/share/fonts/CascadiaCode.zip -d ~/.local/share/fonts
+  fc-cache -fv
+  mkdir -p ~/.config
+  git clone https://gitlab.com/snippets/2295216.git ~/.config/bash
+  git clone https://gitlab.com/snippets/2351345.git ~/.config/oh-my-posh
+  wget -qO- https://ohmyposh.dev/install.sh | sudo bash -s
+  echo \"source ~/.config/bash/bash_aliases\" >> ~/.bashrc
+  # Remove prod pve subscription
+  rm -f /etc/apt/sources.list.d/pve-enterprise.list
+  # Remove prod ceph subscription
+  rm -f /etc/apt/sources.list.d/ceph.list
+  # Create new sources list
+  echo "deb http://download.proxmox.com/debian/pve bookworm pve-no-subscription" > /etc/apt/sources.list.d/pve-nosub.list
+  echo -e "END:\thelper"
+}
+
+# Main function
+function main() {
+  echo -e "START:\tmain"
+  echo "Waiting for cloud-init to finish"
+  cloud-init status --wait
+  echo "System information:"
+  echo "Hostname: $(hostname)"
+  echo "CPU usage: $(top -bn1 | grep "Cpu(s)" | sed "s/.*, *\([0-9.]*\)%* id.*/\1/" | awk '{print 100 - $1"%"}')"
+  echo "Memory usage: $(free | grep Mem | awk '{print $3/$2 * 100.0"%"}')"
+  map_arguments "$@"
+  helper
+  echo -e "END:\tmain"
+}
+
+# Start the script
+main "$@"
